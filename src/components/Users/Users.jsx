4cc280a3d47d6f0a59c7e6ec934cf964a01c50a3
fight-4-life <55998000+fight-4-life@users.noreply.github.com>
2020-09-04 20:48:10 +0500

    point 54 componentDidMount

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -4,14 +4,11 @@ import * as axios from 'axios'
 import userPhoto from '../../assets/user.png'
 
 class Users extends React.Component {
-
-    constructor(props) {
-        super(props)
+    componentDidMount() {
         axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
             this.props.setUsers(response.data.items)
         })
     }
-
     render() {
         return (<div>
             {this.props.users.map(u =>
