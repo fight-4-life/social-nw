ce39565831305c36488d4d7bb8614283d16fa675
fight-4-life <55998000+fight-4-life@users.noreply.github.com>
2020-09-25 22:26:12 +0500

    point 79 stopSubmit (redux-form)

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,23 +1,27 @@
 import React from 'react';
-import {Field, reduxForm} from 'redux-form'
+import { Field, reduxForm } from 'redux-form'
 import { Input } from '../common/FormsControls/FormsControls';
 import { required } from '../../validators/validators';
 import { connect } from 'react-redux';
 import { login } from '../../redux/authReducer';
 import { Redirect } from 'react-router-dom';
+import styles from '../common/FormsControls/FormsControls.module.css'
 
 const LoginForm = (props) => {
     return (
         <form onSubmit={props.handleSubmit}>
             <div>
-                <Field placeholder={'Email'} name={'email'} component={Input} validate={[required]}/>
+                <Field placeholder={'Email'} name={'email'} component={Input} validate={[required]} />
             </div>
             <div>
-            <Field placeholder={'Password'} name={'password'} component={Input} validate={[required]} type={'password'}/>
+                <Field placeholder={'Password'} name={'password'} component={Input} validate={[required]} type={'password'} />
             </div>
             <div>
-                <Field type={'checkbox'} name={'rememberMe'} component={Input}/> remember me
+                <Field type={'checkbox'} name={'rememberMe'} component={Input} /> remember me
             </div>
+            {props.error && <div className={styles.formSummaryError}>
+                {props.error}
+            </div>}
             <div>
                 <button>Login </button>
             </div>
@@ -25,7 +29,7 @@ const LoginForm = (props) => {
     )
 }
 
-const LoginReduxForm = reduxForm({form: 'login'})(LoginForm)
+const LoginReduxForm = reduxForm({ form: 'login' })(LoginForm)
 
 const Login = (props) => {
     const onSubmit = (formData) => {
@@ -37,9 +41,9 @@ const Login = (props) => {
     }
 
     return <div>
-    <h1>LOGIN</h1>
-        <LoginReduxForm onSubmit={onSubmit}/>
-        </div>
+        <h1>LOGIN</h1>
+        <LoginReduxForm onSubmit={onSubmit} />
+    </div>
 }
 
 const mapStateToProps = (state) => ({
--- src/redux/authReducer.js
+++ src/redux/authReducer.js
@@ -1,4 +1,5 @@
 import { authAPI } from '../api/api'
+import { stopSubmit } from 'redux-form';
 
 const SET_USER_DATA = 'SET_USER_DATA';
 
@@ -38,6 +39,9 @@ export const login = (email, password, rememberMe) => (dispatch) => {
   .then(response => {
     if (response.data.resultCode === 0) {
       dispatch(getAuthUserData())
+    } else {
+      let messaage = response.data.messages.length > 0 ? response.data.messages[0] : 'Some error!'
+      dispatch(stopSubmit('login', {_error: messaage}))
     }
   })
 }
